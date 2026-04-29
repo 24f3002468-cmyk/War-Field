71d334ec4bddd8c6954486771060f4495ee55392
Sahil Saini <24f3002468@ds.study.iitm.ac.in>
2026-04-29 20:33:22 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// FROM:
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true
-      }
-    }
-  }
-})
-
-// TO:
 export default defineConfig({
   plugins: [react()],
   build: {
