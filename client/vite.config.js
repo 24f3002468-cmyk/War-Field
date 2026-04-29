a59968d0650cdb597d551356f4014113506a96f5
Sahil Saini <24f3002468@ds.study.iitm.ac.in>
2026-04-29 20:20:53 +0530

    Modify Vite config for build output and proxy
    Update Vite configuration to specify output directory and API proxy.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// FROM:
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true
+      }
+    }
+  }
+})
+
+// TO:
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: '../server/public'
+  },
   server: {
     proxy: {
       '/api': {
